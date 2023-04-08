8a7f4ef646c1f4d3cfeccfa9280254d4d7fa881a
flcl88 <gabriel8aguilar8@gmail.com>
2023-04-08 13:34:59 -0400

    Score is saved

--- rock.js
+++ rock.js
@@ -35,11 +35,46 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
+function getPlayerChoice(){
+    let validatedInput = false;
+    while(validatedInput == false){
+        const choice = prompt("Rock Paper Scissors");
+        if(choice == null){
+            continue;
+        }
+        const choiceInLower = choice.toLowerCase();
+        if(options.includes(choicesInLower)){
+            validatedInput = true;
+            return choiceInLower;
+        }
+    }
+}
+
+
 function game(){
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    console.log("Welcome!!")
     for (let i = 0; i < 5; i++) {
-        const playerSelection = "rock";
+        const playerSelection = getPlayerChoice();
         const computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+        if(checkWinner(playerSelection, computerSelection) == "Player"){
+            scorePlayer++
+        }
+        else if(checkWinner(playerSelection, computerSelection) == "Computer"){
+            scoreComputer++;
+        }
+    }
+    console.log("Game Over")
+    if(scorePlayer > scoreComputer){
+        console.log("Player was the winner")
+    }
+    else if(scorePlayer < scoreComputer){
+        console.log("Computer was the winner");
+    }
+    else{
+        console.log("We have a tie!");
     }
 }
 
